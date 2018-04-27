1e5f97915188a535e22855daddb84aa1e2ec73dd
Windows10CE <windows10ce@gmail.com>
2018-04-26 20:33:59 -0500

    Copied some stuff fro my other bot

--- ctOS_Moderation/Program.cs
+++ ctOS_Moderation/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.IO;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace ctOS_Moderation
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _service;
+
+        public async Task RunBotAsync() {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _service = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = File.ReadAllText("Token");
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(3000);
+            Console.WriteLine("");
+
+            await Task.Delay(-1);
+        }
+
+        public Task Log(LogMessage arg) {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync() {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task HandleCommandAsync(SocketMessage s) {
+            var message = s as SocketUserMessage;
+            if (message == null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("ctOS!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _service);
+
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand) {
+                    Console.WriteLine(result.ErrorReason);
+                    Console.WriteLine("");
+                }
+            }
         }
     }
 }
