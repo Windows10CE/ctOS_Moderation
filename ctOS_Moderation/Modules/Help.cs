c91b69b6bb287e40e6e271a16b1f24591f9e755c
Windows10CE <windows10ce@gmail.com>
2018-04-27 02:38:51 -0500

    Added "cm.warn show" and "cm.help"

---
+++ ctOS_Moderation/Modules/Help.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Discord;
+using System.Threading.Tasks;
+
+namespace ctOS_Moderation.Modules {
+    public class Help : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        public async Task HelpAsync() {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder
+                .AddField("help", "This help menu!")
+                .AddField("ping", "Pong!")
+                .AddField("warn", "Warning system.")
+                .AddInlineField("warn add [user mention] [warning text]", "Addes a warning to a user, requires you to have a role called \"ctOS Warnings\"")
+                .AddInlineField("warn show [user mention]", "Shows all the warns a user has across all servers. Also requires you to have a role called \"ctOS Warnings\"")
+                .WithColor(Color.DarkBlue);
+
+            await ReplyAsync("", false, builder.Build());
+        }
+    }
+}
--- ctOS_Moderation/Modules/Warning.cs
+++ ctOS_Moderation/Modules/Warning.cs
@@ -37,9 +37,9 @@ namespace ctOS_Moderation.Modules
                 string usernameOfWarned = $"@{userToWarn.Username}#{userToWarn.Discriminator}";
 
                 JObject warningObj = new JObject(
-                    new JProperty("Warning Server", Context.Guild.Name),
+                    new JProperty("Warning Location", $"{Context.Guild.Name} in channel #{Context.Channel.Name}"),
                     new JProperty("Warning", warning),
-                    new JProperty("Given By", usernameOfWarned));
+                    new JProperty("Given By", $"@{Context.User.Username}#{Context.User.Discriminator}"));
                 string warningJSONString = warningObj.ToString();
 
                 File.AppendAllText(userWarningsFile, warningJSONString);
@@ -59,6 +59,71 @@ namespace ctOS_Moderation.Modules
                 await UserExtensions.SendMessageAsync(userToDM, "You have been warned.", false, builderDM.Build());
             } else {
                 await ReplyAsync("You don't have the needed role. (ctOS Warnings)");
+            }
+        }
+        [Command("show")]
+        public async Task ShowWarningsAsync(SocketGuildUser userToCheck) {
+            var user = Context.User as SocketGuildUser;
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "ctOS Warnings");
+            if (user.Roles.Contains(role)) {
+                string filename = $"c:\\Users\\aaron\\Documents\\Moderation\\{userToCheck.Id}.json";
+
+                if (File.Exists(filename)) {
+                    List<JObject> warnings = GetJSONObjects(filename);
+                    EmbedBuilder warningsToDisplay = new EmbedBuilder();
+
+                    int numOfWarnings = 0;
+                    foreach (JObject JObj in warnings) {
+                        numOfWarnings++;
+                        warningsToDisplay
+                            .AddField($"Warning {numOfWarnings.ToString()}:", GetJObjectValue(JObj, "Warning"))
+                            .AddInlineField("Given By:", GetJObjectValue(JObj, "Given By"))
+                            .AddInlineField("Given In:", GetJObjectValue(JObj, "Warning Location"));
+                    }
+
+                    await ReplyAsync($"User @{userToCheck.Username}#{userToCheck.Discriminator} has {numOfWarnings.ToString()} warning(s).", false, warningsToDisplay.WithColor(Color.DarkRed).Build());
+                } else {
+                    await ReplyAsync($"{userToCheck.Username}#{userToCheck.Discriminator} has no warnings.");
+                }
+            }
+        }
+
+        public List<JObject> GetJSONObjects(string filename) {
+            int BracketCount = 0;
+            string JSONString = File.ReadAllText(filename);
+            List<string> JsonItems = new List<string>();
+            StringBuilder Json = new StringBuilder();
+
+            foreach (char c in JSONString) {
+                if (c == '{')
+                    ++BracketCount;
+                else if (c == '}')
+                    --BracketCount;
+                Json.Append(c);
+
+                if (BracketCount == 0 && c != ' ') {
+                    JsonItems.Add(Json.ToString());
+                    Json = new StringBuilder();
+                }
+            }
+            List<JObject> JObjs = new List<JObject>();
+
+            foreach(string JObj in JsonItems) {
+                JObjs.Add(JObject.Parse(JObj));
+            }
+            return JObjs;
+        }
+        string GetJObjectValue(JObject array, string key) {
+            foreach (KeyValuePair<string, JToken> keyValuePair in array) {
+                if (key == keyValuePair.Key) {
+                    return keyValuePair.Value.ToString();
+                }
+            }
+            if (key == "Threat Level") {
+                return "No Threat Level Data Found";
+            } else {
+                Console.WriteLine($"Error, no key found for {key}");
+                return String.Empty;
             }
         }
     }
