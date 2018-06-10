0862744111cf3e548a17cbc121f52b7fe332f604
Windows10CE <windows10ce@gmail.com>
2018-06-09 22:48:29 -0500

    Added cm.embedbuild

---
+++ ctOS_Moderation/Modules/EmbedBuild.cs
@@ -0,0 +1,60 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctOS_Moderation.Modules {
+    public class EmbedBuild : ModuleBase<SocketCommandContext> {
+        [Command("embedbuild")]
+        public async Task EmbedBuildAsync([Remainder]string args) {
+            if (!(Context.User as IGuildUser).GuildPermissions.ManageMessages) {
+                await ReplyAsync("You must have the Manage Messages permission!");
+                return;
+            }
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            List<EmbedItem> embedItems = new List<EmbedItem>();
+
+            StringBuilder EmbedItemBuilder = new StringBuilder();
+            int numOfQuotations = 0;
+            foreach (char c in args) {
+                if (c == '\"')
+                    numOfQuotations++;
+                if ((numOfQuotations == 0 || numOfQuotations == 2) && c != ':')
+                    continue;
+                if (numOfQuotations == 4) {
+                    EmbedItemBuilder.Append(c);
+                    string[] EmbedItemString = EmbedItemBuilder.ToString().Replace("\"", "").Split(':');
+                    if (EmbedItemString.Length != 2) {
+                        await ReplyAsync("An unknown error has occured");
+                        return;
+                    }
+                    embedItems.Add(new EmbedItem {
+                        EmbedTitle = EmbedItemString[0],
+                        EmbedContent = EmbedItemString[1]
+                    });
+                    EmbedItemBuilder = new StringBuilder();
+                    numOfQuotations = 0;
+                    continue;
+                }
+                EmbedItemBuilder.Append(c);
+            }
+
+            if (!(embedItems.Count > 0)) {
+                await ReplyAsync("Please use the correct format for embeds");
+                return;
+            }
+
+            foreach (EmbedItem item in embedItems)
+                builder.AddField(item.EmbedTitle, item.EmbedContent);
+
+            await ReplyAsync("in dev", false, builder.Build());
+        }
+        public class EmbedItem {
+            public string EmbedTitle;
+            public string EmbedContent;
+        }
+    }
+}
--- ctOS_Moderation/Modules/Help.cs
+++ ctOS_Moderation/Modules/Help.cs
@@ -15,7 +15,7 @@ namespace ctOS_Moderation.Modules {
                 .AddInlineField("kick [user mention]", "Kicks the mentioned user.")
                 .AddInlineField("ban [user mention] (days to prune) (reason)", "Bans the user with a reason(optional), and deletes their messages over the past however many days(optional).")
                 .AddInlineField("userinfo [user mention]", "Returns a chunk of the mentioned useres info related to Discord and the server you're in.")
-                .AddField("Warnings", "Warning system.")
+                .AddInlineField("embedbuild [embed items, you can have as many as you like]", "The embed item format is [\"Item Title\":\"Item Content\"] without the sqaure brackets. This command requires the Manage Messages permission.")
                 .AddInlineField("show [user mention] (page number)", "Shows the warnings of a user. Requires a role called \"ctOS Warnings\" or the Manage Messages Permission")
                 .AddInlineField("warn [user mention] [warning text]", "Adds a warning to the user. Requires a role called \"ctOS Warnings\" or the Manage Messages Permission")
                 .AddInlineField("delete [user mention] [warning # or \"all\"]", "Deletes a warning message from the user. Requires a role called \"ctOS Warnings\" or the Manage Messages permission. Warning: This command will only delete warnings given in the server you send this command in.")
