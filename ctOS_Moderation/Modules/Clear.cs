bb67c20ab25b9ce718d466e8a4ceea676b48dabc
Windows10CE <windows10ce@gmail.com>
2018-06-12 06:36:07 -0500

    Added cm.clear

---
+++ ctOS_Moderation/Modules/Clear.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace ctOS_Moderation.Modules {
+    public class Clear : ModuleBase<SocketCommandContext> {
+        [Command("clear"), RequireBotPermission(GuildPermission.ManageMessages), RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task ClearMessagesAsync(int numOfMesages) {
+            var messages = await Context.Channel.GetMessagesAsync(numOfMesages + 1).Flatten();
+
+            await Context.Channel.DeleteMessagesAsync(messages);
+
+            IUserMessage reply = await ReplyAsync("The messages have been deleted!");
+            await Task.Delay(3000);
+            await reply.DeleteAsync();
+        }
+    }
+}
