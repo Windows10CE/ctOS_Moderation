1b64a5fc3a98e6122e5d75d31227e75061a081cd
Windows10CE <windows10ce@gmail.com>
2018-06-12 04:53:25 -0500

    Added the ability to list all the servers the bot is in

---
+++ ctOS_Moderation/Modules/ListServers.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace ctOS_Moderation.Modules {
+    public class ListServers : ModuleBase<SocketCommandContext> {
+        [Command("listservers"), RequireOwner]
+        public async Task GetServerListAsync() {
+            string filename = Path.Combine(StaticValues.CTOSModDir, "temp.txt");
+            StringBuilder builder = new StringBuilder();
+
+            foreach (SocketGuild server in Context.Client.Guilds)
+                builder.Append(server.Name + "\n");
+
+            File.WriteAllText(filename, builder.ToString());
+            await Context.Channel.SendFileAsync(filename, "These are all the servers this bot is currently in.");
+            File.Delete(filename);
+        }
+    }
+}
